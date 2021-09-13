409da8b9788bee1998dc8ff03a6f94bbeb6d80f3
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-09-12 21:36:45 -0400

    show instructions for scanner while in conning tower

--- ReturnOfTheAncients/Mono/Modules/CyclopsScanner.cs
+++ ReturnOfTheAncients/Mono/Modules/CyclopsScanner.cs
@@ -1,3 +1,4 @@
+using ArchitectsLibrary.Utility;
 using UnityEngine;
 
 namespace RotA.Mono.Modules
@@ -6,11 +7,28 @@ namespace RotA.Mono.Modules
     {
         private SubRoot _cyclops;
         private CyclopsExternalCams _cyclopsExternalCams;
+        private bool _showedInstructions;
 
         void Start()
         {
             _cyclops = GetComponent<SubRoot>();
             _cyclopsExternalCams = GetComponentInChildren<CyclopsExternalCams>();
+        }
+
+        void Update()
+        {
+            if (_cyclopsExternalCams != null && _cyclopsExternalCams.usingCamera && _cyclopsExternalCams.cameraIndex == 1)
+            {
+                if (!_showedInstructions)
+                {
+                    ErrorMessage.AddMessage(LanguageCache.GetButtonFormat("CyclopsScannerInstructions", GameInput.Button.AltTool));
+                    _showedInstructions = true;
+                }
+            }
+            else
+            {
+                _showedInstructions = false;
+            }
         }
     }
 }
