5b5daffe79d0db97f6fa8e0062a2bc2fb76c7ee8
Metious <71298690+Metious@users.noreply.github.com>
2021-09-16 07:45:54 +0430

    comment all of LiveMixin_Patches

--- ReturnOfTheAncients/Patches/LiveMixin_Patches.cs
+++ ReturnOfTheAncients/Patches/LiveMixin_Patches.cs
@@ -10,22 +10,25 @@ using Logger = QModManager.Utility.Logger;
 namespace RotA.Patches
 {
     [HarmonyPatch(typeof(LiveMixin))]
-    public class LiveMixin_Patches
+    internal static class LiveMixin_Patches
     {
+        // the LiveMixin.invincible isn't serialized, therefore, we cant assign it to something in the prefab
+        // initialization, so we'd need to patch LiveMixin.Start so it gets assigned to true as soon as the component is active.
         [HarmonyPatch(nameof(LiveMixin.Start))]
         [HarmonyPostfix]
-        static void Start_Postfix(LiveMixin __instance)
+        private static void Start_Postfix(LiveMixin __instance)
         {
             var garg = __instance.GetComponent<GargantuanBehaviour>();
 
-            if (garg is not null)
+            if (garg)
                 __instance.invincible = true;
         }
 
 
+        // Makes invincible anything not get killed.
         [HarmonyPatch(nameof(LiveMixin.Kill))]
         [HarmonyPrefix]
-        static bool Kill_Prefix(LiveMixin __instance)
+        private static bool Kill_Prefix(LiveMixin __instance)
         {
             if (__instance.invincible)
                 return false;
@@ -33,27 +36,51 @@ namespace RotA.Patches
             return true;
         }
 
+        // Patch to activate the green electricity effect of the Architect Electricity.
         [HarmonyPatch(nameof(LiveMixin.TakeDamage))]
         [HarmonyTranspiler]
-        static IEnumerable<CodeInstruction> TakeDamage_Transpiler(IEnumerable<CodeInstruction> instructions)
+        private static IEnumerable<CodeInstruction> TakeDamage_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             List<CodeInstruction> codes = new(instructions);
-            MethodInfo architectElectEffect =
-                typeof(LiveMixin_Patches).GetMethod(nameof(ArchitectElectEffect),
-                    BindingFlags.Public | BindingFlags.Static);
+            
+            // Get the MethodInfo of the method we wanna invoke in this patch
+            var architectElectEffect = AccessTools.Method(typeof(LiveMixin_Patches), nameof(ArchitectElectEffect));
 
             bool found = false;
 
 
+            /*
+             * So what we basically want to do is insert the LiveMixin_Patches.ArchitectElectEffect() method call
+             * above the ProfilingUtils.BeginSample("LiveMixin.TakeDamage.DamageEffect"); call.
+             * to find that IL location, we can check for the "LiveMixin.TakeDamage.DamageEffect" string in the stack,
+             * which is only used in the location we want.
+             *
+             * Since the LiveMixin_Patches.ArchitectElectEffect() method takes a LiveMixin we need to feed it that too,
+             * It also needs to read the DamageType that was passed as an argument in the LiveMixin.TakeDamage() call,
+             * so we need to feed it that as well.
+             *
+             * Note: how IL stack works for methods is that u first push the method arguments to the stack, and then
+             * you do the method call.
+             */
             for (int i = 0; i < codes.Count(); i++)
             {
+                /*
+                 * OpCodes.Ldstr is the opcode of string literals, so we first check for that opcode, then we check
+                 * what the operand is assigned to.
+                 *
+                 * Just in case and to verify that it's in fact used as a method parameter, we check for the Opcodes.Call opcode.
+                 */
                 if (codes[i].opcode == OpCodes.Ldstr &&
                     (string)codes[i].operand == "LiveMixin.TakeDamage.DamageEffect" &&
                     codes[i + 1].opcode == OpCodes.Call)
                 {
                     found = true;
+                    
+                    // we load the current LiveMixin instance to the stack
                     codes.Insert(i + 2, new CodeInstruction(OpCodes.Ldarg_0));
+                    // then the DamageType parameter of LiveMixin.TakeDamage()
                     codes.Insert(i + 3, new CodeInstruction(OpCodes.Ldarg_3));
+                    // and then we call our LiveMixin_Patches.ArchitectElectEffect() method.
                     codes.Insert(i + 4, new CodeInstruction(OpCodes.Call, architectElectEffect));
                     break;
                 }
@@ -67,7 +94,8 @@ namespace RotA.Patches
             return codes.AsEnumerable();
         }
 
-        public static void ArchitectElectEffect(LiveMixin liveMixin, DamageType type)
+        // Yoinks the vanilla electrical damage effect and changes it's color to green.
+        private static void ArchitectElectEffect(LiveMixin liveMixin, DamageType type)
         {
             if (Time.time > liveMixin.timeLastElecDamageEffect + 2.5f && type == Mod.ArchitectElect &&
                 liveMixin.electricalDamageEffect is not null)
